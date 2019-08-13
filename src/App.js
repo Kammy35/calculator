c34d25141fd5cf342ea687c7a003db8c27fac974
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-13 17:02:55 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';//
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+      return (
+        <div className="app">
+          <div className="calc-wrapper">
+            <div className="row">
+              <input></input>
+            </div>
+            <div className="row">
+              <clearButton>Clear</clearButton>
+              <button>/</button>
+            </div>
+            <div className="row">
+              <button>7</button>
+              <button>8</button>
+              <button>9</button>
+              <button>-</button>
+              </div>
+            <div className="row">
+              <button>4</button>
+              <button>5</button>
+              <button>6</button>
+              <button>+</button>
+            </div>
+            <div className="row">
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+              <button>=</button>
+            </div>
+
+          </div>
+
+        </div>
+
+    );
+  }
+  
 }
+
+
+
 
 export default App;
